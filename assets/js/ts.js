35998ceab009a63ecaa1777f53bd362a64486cc5
Jesse Gardner <jesse@plasticmind.com>
2017-04-15 09:44:40 -0400

    Use cookies to only show animation once per visit

--- assets/js/ts.js
+++ assets/js/ts.js
@@ -2,35 +2,38 @@ jQuery(document).ready(function($) {
 
 	$('.site-header .logo a').show();
 
-	if(!Cookies.get('returning_visitor')) {
+	if(Cookies.get('returning_visitor')) {
+
 		var paths = $('.site-header .logo path:not(defs path)');
+		paths.each(function(i, e) {
+		    e.style.strokeDasharray = e.style.strokeDashoffset = e.getTotalLength();
+		});
+		var tl = new TimelineMax();
+
+		var e = Power0.easeIn;
+		var d = 1; 
+
+		tl.add([
+		    TweenLite.to(paths.eq(0), .3, {strokeDashoffset: 0, delay: d+0, ease: e}), // T
+		    TweenLite.to(paths.eq(1), .3, {strokeDashoffset: 0, delay: d+0.3, ease: e}), // T
+		    TweenLite.to(paths.eq(2), 1, {strokeDashoffset: 0, delay: d+.5, ease: e}), // ro
+		    TweenLite.to(paths.eq(3), .5, {strokeDashoffset: 0, delay: d+1.5, ease: e}), // y
+
+		    TweenLite.to(paths.eq(4), .5, {strokeDashoffset: 0, delay: d+2.5}), // S
+		   	TweenLite.to(paths.eq(5), .15, {strokeDashoffset: 0, delay: d+3}), // t
+		    TweenLite.to(paths.eq(6), .75, {strokeDashoffset: 0, delay: d+3.15}), // ori
+		    TweenLite.to(paths.eq(7), .15, {strokeDashoffset: 0, delay: d+3.90}), // i
+		    TweenLite.to(paths.eq(8), .35, {strokeDashoffset: 0, delay: d+4.05}), // es
+
+		]).timeScale(2.5);
+
+		$('.site-header .logo').mouseenter(function() { tl.pause(); });
+		$('.site-header .logo').mouseleave(function() { tl.play(); });	
+
+	} else {
+		Cookies.set('returning_visitor', '1', { expires: 1 });
 	}
 
-	var paths = $('.site-header .logo path:not(defs path)');
-	paths.each(function(i, e) {
-	    e.style.strokeDasharray = e.style.strokeDashoffset = e.getTotalLength();
-	});
-	var tl = new TimelineMax();
-
-	var e = Power0.easeIn;
-	var d = 1; 
-
-	tl.add([
-	    TweenLite.to(paths.eq(0), .3, {strokeDashoffset: 0, delay: d+0, ease: e}), // T
-	    TweenLite.to(paths.eq(1), .3, {strokeDashoffset: 0, delay: d+0.3, ease: e}), // T
-	    TweenLite.to(paths.eq(2), 1, {strokeDashoffset: 0, delay: d+.5, ease: e}), // ro
-	    TweenLite.to(paths.eq(3), .5, {strokeDashoffset: 0, delay: d+1.5, ease: e}), // y
-
-	    TweenLite.to(paths.eq(4), .5, {strokeDashoffset: 0, delay: d+2.5}), // S
-	   	TweenLite.to(paths.eq(5), .15, {strokeDashoffset: 0, delay: d+3}), // t
-	    TweenLite.to(paths.eq(6), .75, {strokeDashoffset: 0, delay: d+3.15}), // ori
-	    TweenLite.to(paths.eq(7), .15, {strokeDashoffset: 0, delay: d+3.90}), // i
-	    TweenLite.to(paths.eq(8), .35, {strokeDashoffset: 0, delay: d+4.05}), // es
-
-	]).timeScale(2.5);
-
-	$('.site-header .logo').mouseenter(function() { tl.pause(); });
-	$('.site-header .logo').mouseleave(function() { tl.play(); });	
 
 });
 
